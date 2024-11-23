fba9c8bde6be40e0214ed2720907cb815f3262e6
MadhaviGit0607 <surepallimadhavi.eee@gmail.com>
2024-11-23 17:18:46 +0530

    Fix Note model path issue

--- app.js
+++ app.js
@@ -7,18 +7,17 @@ require('dotenv').config();
 
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(bodyParser.json());
 
-// Route for the root endpoint ("/")
+
 app.get('/', (req, res) => {
-  res.send('Welcome to the Note API!'); // This will respond when accessing http://localhost:5000/
+  res.send('Welcome to the Note API!'); 
 });
 
-// Routes for notes
 app.use('/api/notes', noteRoutes);
 
-// Start server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
