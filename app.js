9967b3919da08bf59ca488cd870f6720e52b9807
MadhaviGit0607 <162568991+MadhaviGit0607@users.noreply.github.com>
2024-11-23 17:27:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,5 +19,5 @@ app.get('/', (req, res) => {
 app.use('/api/notes', noteRoutes);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
